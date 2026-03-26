ddf76ca25433eb08a61f456cc8a30abccb296ca2
Isis Cedeño <isis01sofic@gmail.com>
2026-03-26 00:46:04 -0400

    personalizacion de archivos para iniciar

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const { PrismaClient } = require('@prisma/client');
+
 
 const app = express();
-const prisma = new PrismaClient();
 const PORT = process.env.PORT || 3000;
 
 // Middlewares
@@ -12,20 +11,11 @@ app.use(express.json()); // Permite recibir datos en formato JSON
 
 // Ruta de prueba
 app.get('/api', (req, res) => {
-  res.json({ mensaje: '¡Bienvenido a la API del Taller Mecánico soy Iverso Vargas!' });
+  res.json({ mensaje: '¡ API del taller mecánico ' });
 });
 
-// Ejemplo: Ruta para obtener todos los clientes
-app.get('/api/clientes', async (req, res) => {
-  try {
-    const clientes = await prisma.cliente.findMany();
-    res.json(clientes);
-  } catch (error) {
-    res.status(500).json({ error: 'Error al obtener los clientes' });
-  }
-});
 
 // Iniciar servidor
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en en puerto ${PORT}`);
 });
