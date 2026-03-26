70684517d9e5c30afcb5354e41476b2e8c7c5df0
Iverson-Vargas <iversonvargas8@gmail.com>
2026-03-26 03:43:40 -0400

    cliente listo

--- index.js
+++ index.js
@@ -10,12 +10,15 @@ const empleadoRoutes = require('./src/routes/empleados');
 const nominaRoutes = require('./src/routes/nomina');
 const ordenesRoutes = require('./src/routes/ordenes');
 const facturasRoutes = require('./src/routes/facturas');
+const authRoutes = require('./src/routes/usuarios');
+const clientesRoutes = require('./src/routes/clientes');
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/empleados', empleadoRoutes);
 app.use('/api/nomina', nominaRoutes);
+app.use('/api/clientes', clientesRoutes);
 
 // Registro de rutas para facturación
 // El frontend llama a estos endpoints en la raíz
@@ -27,43 +30,8 @@ app.get('/api', (req, res) => {
   res.json({ mensaje: '¡Bienvenido a la API del Taller Mecánico!' });
 });
 
-// RUTA PARA EL LOGIN CON CREDENCIALES DE PRUEBA
-app.post('/api/login', async (req, res) => {
-  const { usuario, password } = req.body;
-
-  // --- CREDENCIALES PARA PROBAR ---
-  if (usuario === "admin" && password === "12345") {
-    console.log("Acceso concedido con usuario de prueba");
-    return res.json({ 
-      success: true, 
-      mensaje: "¡Bienvenido (Prueba)!", 
-      empleado: { nombre: "Administrador", cargo: "Jefe" } 
-    });
-  }
-
-  // --- BÚSQUEDA EN BASE DE DATOS  ---
-  try {
-    const empleadoEncontrado = await prisma.empleado.findFirst({
-      where: {
-        usuario: usuario,
-        password: password
-      }
-    });
-
-    if (empleadoEncontrado) {
-      res.json({ 
-        success: true, 
-        mensaje: "¡Bienvenido!", 
-        empleado: { nombre: empleadoEncontrado.nombre, cargo: empleadoEncontrado.cargo } 
-      });
-    } else {
-      res.status(401).json({ success: false, mensaje: "Usuario o contraseña incorrectos" });
-    }
-  } catch (error) {
-    console.error("Error de Prisma:", error.message);
-    res.status(500).json({ success: false, mensaje: "Error en el servidor" });
-  }
-});
+// Registrar rutas de Autenticación
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
   console.log(`==============================================`);
